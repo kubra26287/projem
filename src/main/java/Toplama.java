4c1fc5de40b4b87b6d027485c6481925905e537b
kubra26287 <kubra.26287@gmail.com>
2023-02-22 20:13:06 +0100

    v2

--- src/main/java/Toplama.java
+++ src/main/java/Toplama.java
@@ -1,10 +1,13 @@
 public class Toplama {
     public static void main(String[] args) {
-        topla(3, 2);
+        topla(3, 2,5,-10);
     }
 
-    public static void topla(int a, int b) {
-        System.out.println(a + b);
-
+    public static void topla(int... a) {
+        int top =0;
+        for (int w:a) {
+            top+= w;
+        }
+        System.out.println(top);
     }
 }
